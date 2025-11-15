b25ee753495f5dba2ea4d282822d3987eb2a3b96
Prerana G <gprerana212@gmail.com>
2025-11-15 20:26:22 +0530

    Initial commit: added scores.py

---
+++ score.py
@@ -0,0 +1,18 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Please provide scores as command-line arguments.")
+    sys.exit()
+
+scores = list(map(int, sys.argv[1:]))
+
+total = sum(scores)
+average = total / len(scores)
+maximum = max(scores)
+minimum = min(scores)
+
+print("Scores:", scores)
+print("Sum =", total)
+print("Average =", average)
+print("Maximum =", maximum)
+print("Minimum =", minimum)
